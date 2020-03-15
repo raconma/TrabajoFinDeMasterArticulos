6f013efba7214f5351e495467b830912bf3fd516
Xiang Chen <xchen@foqum.io>
2020-03-15 17:47:48 +0100

    Create notebook from Xiang version

--- inference.py
+++
@@ -1,6 +0,0 @@
-from tfm_articulos import predictor
-
-if __name__ == "__main__":
-
-    sentence = "Fui cine a ver pelicula"
-    print(predictor(sentence))
--- tfm_articulos/consts.py
+++
@@ -1,3 +0,0 @@
-ARTICLES = ["el", "la", "los", "las", "un", "una", "unos", "unas", "lo", "al"]
-RELATIVE_PRONOUNS = ["que", "cual", "cuales"]
-MASK = "[MASK]"
